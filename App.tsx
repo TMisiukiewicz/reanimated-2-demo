4b9f6d7f250306e6ce3d6c3aafae422c5301408f
Tomasz Misiukiewicz <tomasz.misiukiewicz@pagepro.pl>
2020-10-08 19:00:13 +0200

    examples

--- App.tsx
+++ App.tsx
@@ -1,13 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import DragAndSnap from './components/DragAndSnapExample';
+import ChatHeads from './components/ChatHeadsExample';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import ScrollExample from './components/ScrollEventExample';
+
+const Stack = createStackNavigator();
+
+const NavButton = ({screen}: {screen: string}) => {
+    const navigation = useNavigation();
+    return (
+    <TouchableOpacity onPress={() => navigation.navigate(screen)}>
+        <Text>
+            {screen} Example
+        </Text>
+    </TouchableOpacity>
+)}
+
+const HomeScreen = () => {
+    return (
+    <View style={styles.container}>
+        <NavButton screen="DragAndSnap" />
+        <NavButton screen="ChatHeads" />
+        <NavButton screen="ScrollEvent" />
+    </View>
+)}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="DragAndSnap" component={DragAndSnap} />
+            <Stack.Screen name="ChatHeads" component={ChatHeads} />
+            <Stack.Screen name="ScrollEvent" component={ScrollExample} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
